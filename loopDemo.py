d30eb01fc0b3c4a0e406fbd511723863e076d3f4
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-09-27 13:29:43 -0400

    Updated from Brython Server: 9/27/2017 1:29:42 PM

--- loopDemo.py
+++ loopDemo.py
@@ -1,1 +1,13 @@
+#Clay Kynor
+#9/27/17
+#loopDemo.py - intro to loops
 
+"""
+#print I love computer science 5 times
+for i in range(1,6):
+    print("I love Computer Science")"""
+
+#print the numbers for 1-20
+for i in range(1,21):
+    print(i)
+    
