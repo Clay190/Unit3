205fb9d19419b2000f072405dc6fb51ae68497fa
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-10-04 13:47:10 -0400

    Updated from Brython Server: 10/4/2017 1:47:09 PM

--- warmup9.py
+++ warmup9.py
@@ -1,1 +1,11 @@
+#Clay Kynor
+#10/4/17
+#stringLoopDemo.py - how to loop through a string
 
+word = input("Enter a word ")
+
+for ch in word:
+    if ch=="a" or ch=="e" or ch=="i" or ch=="o" or ch=="u":
+        print (ch.upper())
+    else:
+        print(ch)
