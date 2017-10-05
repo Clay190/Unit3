03b4d260ad3569874dda1e494d658c94e535b296
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-10-05 14:28:44 -0400

    Updated from Brython Server: 10/5/2017 2:28:43 PM

--- dotDemo.py
+++ dotDemo.py
@@ -1,1 +1,15 @@
+#Clay Kynor
+#10/5/17
+#dotDemo.py - loops and graphics - making some dots
 
+from ggame import *
+
+purple = Color(0xF000FF,1)
+
+dot = CircleAsset(20,LineStyle(1,purple),purple)
+
+for i in range(20):
+    Sprite(dot,(20 + 40*i,20))
+    
+
+App().run()
