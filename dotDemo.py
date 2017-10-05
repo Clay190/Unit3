1cd20851fc2cd21bc16bbfedb9a11a0deb7eed54
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-10-05 14:36:02 -0400

    Updated from Brython Server: 10/5/2017 2:36:01 PM

--- dotDemo.py
+++ dotDemo.py
@@ -6,10 +6,10 @@ from ggame import *
 
 purple = Color(0xF000FF,1)
 
-dot = CircleAsset(20,LineStyle(1,purple),purple)
+dot = RectangleAsset(20,LineStyle(1,purple),purple)
 
-for i in range(20):
-    Sprite(dot,(20 + 40*i,20))
-    
+for j in range(20): #prints the row 20 times
+    for i in range(20): #prints one row of dots
+        Sprite(dot,(20 + 40*i,20 + 40 * j)) 
 
 App().run()
