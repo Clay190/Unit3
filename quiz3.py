8eba5668845169ab368195623e252e125929ac9f
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-10-13 14:27:14 -0400

    Updated from Brython Server: 10/13/2017 2:27:14 PM

--- quiz3.py
+++ quiz3.py
@@ -1,1 +1,25 @@
+#Clay Kynor
+#10/13/17
+#quiz3.py - Quiz on Chapter 3
 
+"""
+for i in range(-5,6):
+    print(i) """
+
+"""
+i=18
+while i <= 32:
+    print(i)
+    i = i+1 """
+   
+""" 
+total = 0
+for i in range(13,332,2):
+    total = total + i
+print(total) """
+
+while True:
+    word = input("Enter a word: ")
+    for ch in word:
+        if "z" in word:
+        
