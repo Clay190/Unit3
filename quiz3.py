8a89081efc200eca3bd511e1e8dfc7c418eea049
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-10-13 14:40:08 -0400

    Updated from Brython Server: 10/13/2017 2:40:07 PM

--- quiz3.py
+++ quiz3.py
@@ -2,24 +2,37 @@
 #10/13/17
 #quiz3.py - Quiz on Chapter 3
 
-"""
-for i in range(-5,6):
-    print(i) """
 
-"""
+for i in range(-5,6):
+    print(i)
+
+
 i=18
 while i <= 32:
     print(i)
-    i = i+1 """
+    i = i+1
    
-""" 
+
 total = 0
 for i in range(13,332,2):
     total = total + i
-print(total) """
+print(total)
 
+
+total = 0 
+
+while total<10:
+    word = input("Enter a word: ")
+    for ch in word:
+        if 'z' in word or "Z" in word:
+            total = 10
+        
+
+#I know my way isn't the cleanest, I tried to do it this following way, but it wouldn't break
+"""
 while True:
     word = input("Enter a word: ")
     for ch in word:
-        if "z" in word:
-        
+        if 'z' in word or "Z" in word:
+            break
+                """
